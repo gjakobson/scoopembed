d72c67d98eb833655620a690f12f63c23c9faced
gjakobson <59399834+gjakobson@users.noreply.github.com>
2024-06-30 14:42:55 -0700

    basic login works

---
+++ src/aws-exports.js
@@ -0,0 +1,12 @@
+// /src/aws-exports.js
+
+const awsmobile = {
+    "aws_project_region": "us-west-2",
+    "region": "us-west-2",
+    "userPoolId": "us-west-2_4RyltqyJ7",
+    "identityPoolId": "us-west-2:cc279e26-fc27-4b69-ab62-b4f5817d615b",
+    "userPoolWebClientId": "76atr62rn4iuk8uhj8r8gt39ms"
+};
+
+
+export default awsmobile;
