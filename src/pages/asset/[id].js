3d9a79c9326f336d8e1130d6fa8d2b3c4a530930
gjakobson <59399834+gjakobson@users.noreply.github.com>
2024-06-30 21:57:45 -0700

    q param passing thru url fix

--- src/pages/asset/[id].js
+++ src/pages/asset/[id].js
@@ -20,23 +20,24 @@ export async function getServerSideProps(context) {
 
 const Asset = ({ id, userID, workspaceID, insightKey, insightID }) => {
   const isChart = id === 'chart';
+  const queryParam = `${userID}:${workspaceID}:${insightID}:${insightKey}`;
 
   return (
     <div>
       <Head>
         <title>Scoop Analytics</title>
         <meta name="description" content="Beautiful, easy data visualization and storytelling" />
-        <link rel="canonical" href={`https://embed.scoopanalytics.com/asset/${id}`} />
+        <link rel="canonical" href={`https://embed.scoopanalytics.com/asset/${id}?q=${queryParam}`} />
         <meta property="og:title" content="Scoop Analytics" />
         <meta property="og:description" content="Beautiful, easy data visualization and storytelling" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={`https://embed.scoopanalytics.com/asset/${id}`} />
+        <meta property="og:url" content={`https://embed.scoopanalytics.com/asset/${id}?q=${queryParam}`} />
         <meta property="og:site_name" content="Scoop Analytics Test" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
         {/* Custom link for iframely app */}
-        <link rel="iframely app" href={`https://embed.scoopanalytics.com/asset/${id}?userID=${userID}&workspaceID=${workspaceID}&insightKey=${insightKey}&insightID=${insightID}`} media="height=300,scrolling=no" />
-        <link rel="alternate" type="application/json+oembed" href={`https://embed.scoopanalytics.com/api/oembed/${id}?format=json&url=https://embed.scoopanalytics.com/asset/${id}?userID=${userID}&workspaceID=${workspaceID}&insightKey=${insightKey}&insightID=${insightID}`} />
+        <link rel="iframely app" href={`https://embed.scoopanalytics.com/asset/${id}?q=${queryParam}`} media="height=300,scrolling=no" />
+        <link rel="alternate" type="application/json+oembed" href={`https://embed.scoopanalytics.com/api/oembed/${id}?format=json&url=https://embed.scoopanalytics.com/asset/${id}?q=${queryParam}`} />
       </Head>
       <AuthenticatedContent isChart={isChart} id={id} userID={userID} workspaceID={workspaceID} insightID={insightID} insightKey={insightKey} />
     </div>
